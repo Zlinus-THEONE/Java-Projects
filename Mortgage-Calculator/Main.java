ce5cb3a372d4137491677af237810bc99492eca2
Zoren Dungca <dungcazorenlinus@gmail.com>
2026-06-12 13:22:56 +0800

    Add files via upload

--- Mortgage-Calculator/Main.java
+++ Mortgage-Calculator/Main.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        final byte PERCENT = 100;
+        final byte MONTHS_IN_YEAR = 12;
+
+        System.out.println("Mortgage Calculator");
+        System.out.println("-----------------------");
+
+        System.out.print("Principal = ");
+        double principal = input.nextDouble();
+
+        System.out.print("Annual Interest Rate % = ");
+        double interestRate = input.nextDouble();
+        interestRate = interestRate / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.print("Period/Years = ");
+        double periodYears = input.nextDouble();
+        periodYears *= MONTHS_IN_YEAR;
+
+        double mortgage = principal*((interestRate * Math.pow(1+interestRate, periodYears)) / (Math.pow(1+interestRate, periodYears) - 1));
+
+        System.out.println("-----------------------");
+        System.out.println("Mortgage = $" + mortgage);
+        System.out.println("-----------------------");
+    }
+}
