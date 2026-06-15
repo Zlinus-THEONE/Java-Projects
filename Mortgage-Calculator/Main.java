5da4005df54a29dd5b5f31ef0992756a22fc3e9f
Zoren Dungca <dungcazorenlinus@gmail.com>
2026-06-15 17:57:14 +0800

    Update Main.java

--- Mortgage-Calculator/Main.java
+++ Mortgage-Calculator/Main.java
@@ -6,24 +6,70 @@ public class Main {
         final byte PERCENT = 100;
         final byte MONTHS_IN_YEAR = 12;
 
+        boolean loop = true;
+        double principal;
+        double interestRate;
+        double periodYears;
+        String restart;
+
         System.out.println("Mortgage Calculator");
         System.out.println("-----------------------");
 
-        System.out.print("Principal = ");
-        double principal = input.nextDouble();
+        while (loop) {
+            System.out.println();
+            System.out.println("*******************************");
+            while (true) {
+                System.out.print("Principal = ");
+                principal = input.nextDouble();
 
-        System.out.print("Annual Interest Rate % = ");
-        double interestRate = input.nextDouble();
-        interestRate = interestRate / PERCENT / MONTHS_IN_YEAR;
+                if (principal < 1000 || principal > 1000000 ) {
+                    System.out.println("Enter a value greater or equals to $1000 and value less than $1M");
+                } else {break;}
+            }
 
-        System.out.print("Period/Years = ");
-        double periodYears = input.nextDouble();
-        periodYears *= MONTHS_IN_YEAR;
+            while (true) {
+                System.out.print("Annual Interest Rate % = ");
+                interestRate = input.nextDouble();
 
-        double mortgage = principal*((interestRate * Math.pow(1+interestRate, periodYears)) / (Math.pow(1+interestRate, periodYears) - 1));
 
+                if (interestRate > 30 || interestRate <= 0 ) {
+                    System.out.println("Enter a value less than 0 and 30");
+                } else {
+                    interestRate = interestRate / PERCENT / MONTHS_IN_YEAR;
+                    break;
+                }
+            }
+
+            while (true) {
+                System.out.print("Period/Years = ");
+                periodYears = input.nextDouble();
+
+                if (periodYears > 30 || periodYears <= 0 ) {
+                    System.out.println("Enter a value between 1 and 30");
+                } else {
+                    periodYears *= MONTHS_IN_YEAR;
+                    break;
+                }
+            }
+
+            double mortgage = principal*((interestRate * Math.pow(1+interestRate, periodYears)) / (Math.pow(1+interestRate, periodYears) - 1));
+
+            System.out.println("-----------------------");
+            System.out.println("Mortgage = $" + mortgage);
+            System.out.println("-----------------------");
+            System.out.println();
+
+            System.out.print("To continue, enter Y = ");
+            input.nextLine();
+            restart = input.nextLine().toLowerCase().trim();
+
+            if (!restart.equals("y")) {
+                loop = false;
+            }
+        }
+
+        System.out.println();
         System.out.println("-----------------------");
-        System.out.println("Mortgage = $" + mortgage);
-        System.out.println("-----------------------");
+        System.out.println("Thank you for using our mortgage calculator!");
     }
 }
